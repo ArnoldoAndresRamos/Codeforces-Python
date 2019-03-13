23c7e7068ce730e69849abebfcbeae4a4e68872a
Arnoldo Andrés Ramos Acevedo <43187167+ArnoldoAndresRamos@users.noreply.github.com>
2019-03-13 07:31:34 -0300

    Update 478 A. Initial Bet.py
    corregido problema se agrega un if  total = 0

--- 478 A. Initial Bet.py
+++ 478 A. Initial Bet.py
@@ -6,9 +6,12 @@
 c1,c2,c3,c4,c5=list(map(int,input().split()))
 
 total=c1+c2+c3+c4+c5
-if total%5==0:
-    resul=total/5
+
+if total == 0:
+    resul = -1
+elif total%5 == 0:
+    resul = total/5
 else:
-    resul=-1
+    resul = -1
     
 print(int(resul))
