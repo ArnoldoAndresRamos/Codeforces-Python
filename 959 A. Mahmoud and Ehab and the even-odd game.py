fcee6c9dd898596a233262bfc8c14db0974ab20a
Arnoldo Andrés Ramos Acevedo <43187167+ArnoldoAndresRamos@users.noreply.github.com>
2019-03-15 09:25:01 -0600

    Create 959 A. Mahmoud and Ehab and the even-odd game.py

---
+++ 959 A. Mahmoud and Ehab and the even-odd game.py
@@ -0,0 +1,12 @@
+
+#Problema 959 A
+#Math *500
+#https://codeforces.com/problemset/problem/959/A
+#Mahmoud and Ehab and the even-odd game
+
+n=int(input())
+modn=n%2
+if modn==1:
+    print("Ehab")
+else:
+    print("Mahmoud")
